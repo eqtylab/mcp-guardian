95d8a93a558240a0f16244582442539527bb405b
cameronfyfe <cameron.j.fyfe@gmail.com>
2025-02-11 00:51:51 -0800

    add ui nav

--- mcp-guardian/src/App.tsx
+++ mcp-guardian/src/App.tsx
@@ -1,61 +1,45 @@
-import { useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
+import React, { useState } from "react";
+import PendingMessages from "./pages/PendingMessages";
 import "./App.css";
 
+
+const PAGES = [
+  {
+    label: "MCP Servers",
+    component: () => <div>TODO</div>,
+  },
+  {
+    label: "Guard Profiles",
+    component: () => <div>TODO</div>,
+  },
+  {
+    label: "Pending Messages",
+    component: PendingMessages,
+  },
+];
+
 const App = () => {
-  const [pendingApprovals, setPendingApprovals] = useState({} as any);
-
-  const updatePendingApprovals = async () => {
-    const newPendingApprovals = await invoke("get_pending_messages", {});
-    setPendingApprovals(newPendingApprovals);
-  };
-
-  useEffect(() => {
-    updatePendingApprovals();
-  }, []);
-
-  const handleApprove = async (id: string) => {
-    await invoke("approve_message", { id })
-    updatePendingApprovals();
-  };
-
-  const handleDeny = async (id: string) => {
-    await invoke("deny_message", { id })
-    updatePendingApprovals();
-  };
+  const [pageIndex, setPageIndex] = useState(2);
 
   return (
-    <main className="container">
-      <h1>MCP Guardian</h1>
-      
-      <button onClick={updatePendingApprovals}>Refresh</button>
-
-      <div>
-        <h2>Pending Transmissions</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Direction</th>
-              <th>Message</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.entries(pendingApprovals).map(([id, value]: [string, any], i) => {
-              const direction = id.split("_")[0] === "inbound" ? "Inbound" : "Outbound";
-              const message = direction === "Inbound" ? value.result.content : value;
-              return (
-                <tr key={`tr-${i}`}>
-                <td key={`td1-${i}`}>{direction}</td>
-                <td key={`td2-${i}`}>{JSON.stringify(message)}</td>
-                <td key={`td3-${i}`}>
-                  <button className="approve" onClick={() => handleApprove(id)} key={`btn1-${i}`}>Approve</button>
-                  <button className="deny" onClick={() => handleDeny(id)} key={`btn2-${i}`}>Deny</button>
-                  </td>
-               </tr>
-            )})}
-          </tbody>
-        </table>
+    <main>
+      <div className="main-grid">
+        <div className="nav-link-container">
+          {PAGES.map((page, i) => (
+            <div
+              key={`nav-link-${i}`}
+              className={pageIndex === i ? "nav-link active" : "nav-link"}
+              onClick={() => setPageIndex(i)}
+            >
+              {page.label}
+            </div>
+          ))}
+        </div>
+        <div>
+          <div className="page">
+            {React.createElement(PAGES[pageIndex].component)}
+          </div>
+        </div>
       </div>
     </main>
   );
