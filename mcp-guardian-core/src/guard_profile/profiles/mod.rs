6fe8bbdee1d96e8ecad5d97aa0f2313580937ed1
cameronfyfe <cameron.j.fyfe@gmail.com>
2025-02-13 23:40:17 -0800

    update core guard profiles

---
+++ mcp-guardian-core/src/guard_profile/profiles/mod.rs
@@ -0,0 +1,17 @@
+pub static CORE_NAMESPACE: &str = "mcp-guardian";
+
+pub static CORE_PROFILES: &[(&str, &str)] = &[
+    (
+        "approve-tool-call-requests-and-responses",
+        include_str!("./approve-tool-call-requests-and-responses.json"),
+    ),
+    (
+        "approve-tool-call-requests",
+        include_str!("./approve-tool-call-requests.json"),
+    ),
+    (
+        "approve-tool-call-responses",
+        include_str!("./approve-tool-call-responses.json"),
+    ),
+    ("log-only", include_str!("./log-only.json")),
+];
